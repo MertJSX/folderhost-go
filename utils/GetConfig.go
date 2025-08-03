68e76cf32dccbea149bd013a7f0beeeb532d713f
MertJSX <mertJSX@proton.me>
2025-08-03 23:44:20 +0300

    Added GetConfig util

---
+++ utils/GetConfig.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"log"
+	"os"
+
+	"github.com/MertJSX/folder-host-go/types"
+	"gopkg.in/yaml.v3"
+)
+
+func GetConfig() types.ConfigFile {
+	fileData, err := os.ReadFile("./config.yml")
+	if err != nil {
+		log.Fatal("Error reading config file:", err)
+	}
+	var config types.ConfigFile
+
+	err = yaml.Unmarshal(fileData, &config)
+	if err != nil {
+		log.Fatalf("Config.yml parse error: %v", err)
+	}
+
+	return config
+}
