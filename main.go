624a86b540accf02b49e5eac6429c8c6d81b230c
MertJSX <mertJSX@proton.me>
2025-08-03 23:46:36 +0300

    Use GetConfig() util and use CheckAuth middleware

--- main.go
+++ main.go
@@ -2,37 +2,30 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/MertJSX/folder-host-go/types"
+	"github.com/MertJSX/folder-host-go/middleware"
 	"github.com/MertJSX/folder-host-go/utils"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"gopkg.in/yaml.v3"
 )
 
 func main() {
 	app := fiber.New()
 	app.Use(cors.New())
 
-	fileData, err := os.ReadFile("./config.yml")
-	if err != nil {
-		log.Fatal("Error reading config file:", err)
-	}
-	var config types.ConfigFile
+	config := utils.GetConfig()
 
-	err = yaml.Unmarshal(fileData, &config)
-	if err != nil {
-		log.Fatalf("YAML parse error: %v", err)
-	}
 	var PORT string = fmt.Sprintf(":%d", config.Port)
 
 	_, dirSize, _ := utils.GetDirectorySize(config.Folder)
 
 	fmt.Printf("%s \n", dirSize)
 
-	utils.GetDirectoryItems(config.Folder, "Test", config)
+	// utils.GetDirectoryItems(config.Folder, "Test", config)
+
+	app.Use("/api", func(c *fiber.Ctx) error {
+		return middleware.CheckAuth(c)
+	})
 
 	app.Static("/", "./client")
 
