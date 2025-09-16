9c18521073937256f94f80d2bffa93e686dc2852
MertJSX <mertJSX@proton.me>
2025-09-16 20:47:42 +0300

    chore: Add comments and improve error handling.

--- routes/Delete.go
+++ routes/Delete.go
@@ -10,6 +10,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// Existing problems that I will fix:
+// - You can't move the folder or file to recovery_bin if it already exists
 func Delete(c *fiber.Ctx) error {
 
 	if !c.Locals("account").(types.Account).Permissions.Delete {
@@ -89,9 +91,9 @@ func Delete(c *fiber.Ctx) error {
 
 	err = os.Rename(path, fmt.Sprintf("./recovery_bin/%s", itemName))
 
-	if err == nil {
-		return c.Status(200).JSON(fiber.Map{"response": "Item was deleted successfully!"})
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"err": "Error deleting item"})
 	}
 
-	return c.Status(500).JSON(fiber.Map{"err": "Unknown server error"})
+	return c.Status(200).JSON(fiber.Map{"response": "Item was deleted successfully!"})
 }
