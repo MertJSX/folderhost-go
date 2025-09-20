17d208faeb86b296b358091bb9d0614d5933ccf3
MertJSX <mertJSX@proton.me>
2025-09-20 22:33:52 +0300

    feat: Add RecoveryRecord type

---
+++ types/RecoveryRecord.go
@@ -0,0 +1,12 @@
+package types
+
+type RecoveryRecord struct {
+	Id          int    `json:"id,omitempty"`
+	Username    string `json:"username"`
+	OldLocation string `json:"oldLocation"`
+	BinLocation string `json:"binLocation"`
+	IsDirectory bool   `json:"isDirectory"`
+	SizeDisplay string `json:"sizeDisplay"`
+	SizeBytes   int64  `json:"sizeBytes"`
+	CreatedAt   string `json:"created_at"`
+}
