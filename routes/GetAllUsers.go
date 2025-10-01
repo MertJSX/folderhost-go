cfa6e31347967b3ac1445e009c0dc366f759d394
MertJSX <mertJSX@proton.me>
2025-10-01 20:53:25 +0300

    refactor: check permission

--- routes/GetAllUsers.go
+++ routes/GetAllUsers.go
@@ -2,10 +2,16 @@ package routes
 
 import (
 	"github.com/MertJSX/folder-host-go/database/users"
+	"github.com/MertJSX/folder-host-go/types"
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetAllUsers(c *fiber.Ctx) error {
+	if !c.Locals("account").(types.Account).Permissions.Delete {
+		return c.Status(403).JSON(
+			fiber.Map{"err": "No permission!"},
+		)
+	}
 
 	users, err := users.GetAll()
 
