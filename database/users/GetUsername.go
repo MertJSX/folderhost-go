d37e355827c03c2f9cf50a56aee76383da0243cd
MertJSX <mertJSX@proton.me>
2025-10-07 18:10:09 +0300

    feat: create /users/GetUsername

---
+++ database/users/GetUsername.go
@@ -0,0 +1,32 @@
+package users
+
+import (
+	"database/sql"
+
+	"github.com/MertJSX/folder-host-go/database"
+)
+
+func GetUsername(id int) (string, error) {
+	const query = `
+		SELECT
+			username
+		FROM users
+		WHERE id = ?
+	`
+
+	row := database.DB.QueryRow(query, id)
+
+	var username string
+
+	err := row.Scan(
+		&username,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", sql.ErrNoRows
+		}
+		return "", err
+	}
+
+	return username, nil
+}
