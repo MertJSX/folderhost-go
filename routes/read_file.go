807e0b7813e74aeba4861f6d8dc8b4a47f9e78ac
MertJSX <mertJSX@proton.me>
2025-10-24 22:41:56 +0300

    refactor: small error handling

--- routes/read_file.go
+++ routes/read_file.go
@@ -6,15 +6,9 @@ import (
 
 	"github.com/MertJSX/folder-host-go/types"
 	"github.com/MertJSX/folder-host-go/utils"
-	"github.com/MertJSX/folder-host-go/utils/cache"
 	"github.com/gofiber/fiber/v2"
 )
 
-// Known problems:
-//   - Having problems with big files. Needs size limitation.
-//     If someone tries to read a file that has 1 GB size, your PC will crash xd.
-//   - Real-time checking filesize.
-//   - We must ignore big editor-change requests from WebSocket connections.
 func ReadFile(c *fiber.Ctx) error {
 	var path string
 	var fileName string
@@ -58,16 +52,11 @@ func ReadFile(c *fiber.Ctx) error {
 		return c.Status(413).JSON(fiber.Map{"err": "Not enough storage space to edit! Try to close unused CodeEditor windows. Each code editor window guarantees itself 200 KB of space."})
 	}
 
-	var content string
-	var ok bool
+	content, err := os.ReadFile(fmt.Sprintf("%s%s", config.Folder, path))
 
-	if content, ok = cache.FileContentCache.Get(fmt.Sprintf("%s%s", config.Folder, path)); !ok {
-		fileContent, err := os.ReadFile(fmt.Sprintf("%s%s", config.Folder, path))
-		content = string(fileContent)
-
-		if err != nil {
-			return c.Status(500).JSON(fiber.Map{"err": "Error while reading file!"})
-		}
+	if err != nil {
+		fmt.Printf("Error while reading file: %v\n", err)
+		return c.Status(500).JSON(fiber.Map{"err": "Error while reading file!"})
 	}
 
 	return c.Status(200).JSON(fiber.Map{
